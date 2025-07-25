261ce6bc66eb89e2e69376143e58d7ed70966d74
Astemir <akaitov11111@icloud.com>
2025-07-25 16:28:44 +0300

    Landing commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('btnRecord').addEventListener('click', () => {
+    // Заменим ссылку на реального бота, когда он будет готов
+    window.open('https://t.me/informatika_s_astemirom_bot', '_blank');
+  });
